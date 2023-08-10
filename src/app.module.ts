8e5f2d0559ecd010c8a84ff67e1cd9ac203aca55
AnggaDanarP <anggadanarp@gmail.com>
2023-08-10 16:21:10 +0700

    add cats/todo module but still got big error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare const module: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(5000).then(() => {
+    console.log('successfully started on port 5000');
+  });
 }
+
 bootstrap();
